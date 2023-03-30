15dc09efeb5bde5cda8e0423e30298bf135e669d
Liam-Coulson <coulsonl2209@gmail.com>
2023-03-30 17:41:35 +0100

    Solve exercise 04 removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, someArguments) {
+    let argArray = Array.from(arguments);
+    // For every argument, we see if array contains that argument
+    // If it does, we remove it from the array.
+    for (i = 0; i < argArray.length; i++) {
+        let index = array.indexOf(argArray[i])
+        if (index != -1) {
+            array.splice(index, 1)
+        }
+    }
+    return array
 };
 
 // Do not edit below this line
