1695b9bd9520063c374f835adfc8117db2d4b8b0
Liam-Coulson <coulsonl2209@gmail.com>
2023-03-30 20:48:26 +0100

    Solve exercise 05 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let runningSum = 0;
 
+    if (typeof num1 != "number" || typeof num2 != "number") {
+        return "ERROR"
+    }
+
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR"
+    }
+
+
+    if (num1 < num2) {
+        for (i = num1; i <= num2; i++) {
+            runningSum += i;
+        }
+    }
+    else {
+        for (i = num2; i <= num1; i++) {
+            runningSum += i;
+        }
+    }
+
+    return runningSum;
 };
 
 // Do not edit below this line
