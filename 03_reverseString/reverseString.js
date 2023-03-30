452ab34143271f84285828d702bbbcf81072548e
Liam-Coulson <coulsonl2209@gmail.com>
2023-03-30 16:48:43 +0100

    Solve exercise 03 reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arrayOfString = string.split("");
+    let returnString = "";
+    for (i = string.length -1; i >= 0; i = i-1) {
+        returnString += arrayOfString[i];
+    }
+    return returnString;
 };
 
 // Do not edit below this line
