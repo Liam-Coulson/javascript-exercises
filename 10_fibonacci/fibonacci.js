f4b687ac9b11aef25811c904a980997708a2aa1a
Liam-Coulson <coulsonl2209@gmail.com>
2023-05-18 21:12:14 +0100

    Complete exercise 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 1) {
+        return "OOPS";
+    }
+    var sequence = [1, 1];
+    n = parseInt(n);
+    while (sequence.length < n) {
+        sequence.push(sequence[sequence.length - 2] 
+            + sequence[sequence.length - 1])
+    }
+    return sequence[sequence.length - 1]
 };
 
 // Do not edit below this line
