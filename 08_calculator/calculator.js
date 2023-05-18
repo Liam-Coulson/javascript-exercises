f745c109b88a871c78cdfb8aa8c369fb5b33c391
Liam-Coulson <coulsonl2209@gmail.com>
2023-05-18 17:03:15 +0100

    Complete exercise 08 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+  return numArray.reduce((currentSum, next) => currentSum + next, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  if(numArray.length == 0) {
+    return 0;
+  } else {
+    return numArray.reduce((currentProduct, next) => {
+      return currentProduct * next;
+    }, 1);
+  }
 };
 
-const power = function() {
-	
+const power = function(number, exp) {
+	return number ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num == 0) {return 1;}
+	let count = num - 1;
+  while (count > 0){
+    num *= count;
+    count--;
+  }
+  return num
 };
 
 // Do not edit below this line
