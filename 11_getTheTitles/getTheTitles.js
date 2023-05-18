dc5222aec953e979e9c061580db0a96b16496021
Liam-Coulson <coulsonl2209@gmail.com>
2023-05-18 21:22:14 +0100

    Complete exercise 11 getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let results = books.map(obj => obj.title);
+    return results;
 };
 
 // Do not edit below this line
