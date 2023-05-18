35b5b9f9474d40392077cb606099ab770d4e646d
Liam-Coulson <coulsonl2209@gmail.com>
2023-05-18 18:27:13 +0100

    Complete exercise 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  // The line below uses a regex I found on stackexchange
+  str = str.replace(/[^A-Za-z]+/g, "");
+  str = str.toLowerCase();
+  stringArray = str.split("");
+  reverseArray = str.split("").reverse();
+  for (var i = 0; i < stringArray.length; i++) {
+    if (stringArray[i] != reverseArray[i]) {
+      return false;
+    }
+  }
+//   console.log(stringArray);
+//   console.log(reverseArray);
+  return true;
 };
+
+// console.log(palindromes("ZZZZ car, a man, a maracaz."));
 
 // Do not edit below this line
 module.exports = palindromes;
