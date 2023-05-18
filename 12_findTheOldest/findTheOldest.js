48256dd92a5ee568a7407ac5f6554c8def519411
Liam-Coulson <coulsonl2209@gmail.com>
2023-05-18 22:02:24 +0100

    Complete exercise 12 findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,43 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = people.reduce((currOldest, nextPerson) => {
+        currentYear = new Date().getFullYear();
+        if (!currOldest.hasOwnProperty("yearOfDeath")){
+            currOldestAge = currentYear - currOldest.yearOfBirth;
+        } else {
+        currOldestAge = currOldest.yearOfDeath - currOldest.yearOfBirth;
+        }
 
+        if (!nextPerson.hasOwnProperty("yearOfDeath")){
+            nextPersonAge = currentYear - nextPerson.yearOfBirth;
+        } else {
+            nextPersonAge = nextPerson.yearOfDeath - nextPerson.yearOfBirth;
+        }
+
+        if (nextPersonAge > currOldestAge) {
+            return nextPerson;
+        } else {
+            return currOldest;
+        }
+    })
+    return oldest;
 };
+
+const listofpeople = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
 
 // Do not edit below this line
 module.exports = findTheOldest;
